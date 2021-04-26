05a75ad53e75d723fb44c0b9f87467fdc0778efa
万里涛 <wanlitao_3055601@outlook.com>
2021-04-26 21:45:09 +0800

    add aspnetcore integrate project

---
+++ src/HEF.Extensions.NewLife.RocketMQ.AspNetCore/Extensions/HostedServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NewLife.RocketMQ.Bus;
+
+namespace NewLife.RocketMQ.AspNetCore
+{
+    public static class HostedServiceExtensions
+    {
+        public static IServiceCollection AddRocketMQHostedService(this IServiceCollection services)
+        {
+            return services.AddHostedService((provider) =>
+            {
+                var rocketMQControl = provider.GetRequiredService<IRocketMQControl>();
+
+                return new RocketMQHostedService(rocketMQControl);
+            });
+        }
+    }
+}
---
+++ src/HEF.Extensions.NewLife.RocketMQ.AspNetCore/RocketMQHostedService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+using NewLife.RocketMQ.Bus;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NewLife.RocketMQ.AspNetCore
+{
+    public class RocketMQHostedService : IHostedService
+    {
+        private readonly IRocketMQControl _rocketMQControl;
+
+        public RocketMQHostedService(IRocketMQControl rocketMQControl)
+        {
+            _rocketMQControl = rocketMQControl ?? throw new ArgumentNullException(nameof(rocketMQControl));
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return _rocketMQControl.StartAsync(cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return _rocketMQControl.StopAsync(cancellationToken);
+        }
+    }
+}
